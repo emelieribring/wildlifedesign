ea368b6cb4903909429e38112988bafb595374a3
emelieribring <emelieribring@hotmail.com>
2023-11-10 13:25:08 +0100

    fixed sizing

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
     <>
     <footer>
         <div className='designBy'>
-            <p>WildLife Design <br />  <span> by Emelie Ribring</span> <br />Inspierd <br /> <span>by Tomushka Corvys</span></p>
+            <p>WildLife Design <br /> <span> by Emelie Ribring</span> <br /> <br />Inspired <br /> <span>by Tomushka Corvys</span></p>
         </div>
         <div className='footerText'>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <span>Lorem ipsum dolor.</span></p>
